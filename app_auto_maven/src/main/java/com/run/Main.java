d89ea3dd5e98fd34d7125d8ad93d8fa02e13a663
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2022-08-27 18:44:04 -0300

    upload

--- app_auto_maven/src/main/java/com/run/Main.java
+++ app_auto_maven/src/main/java/com/run/Main.java
@@ -1,3 +1,4 @@
+package com.run;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -5,22 +6,24 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class automation {
-    public static void main(String args[]) throws MalformedURLException {
+import com.screen.*;
+
+public class Main {
+    public static void main(String args[]) throws MalformedURLException, InterruptedException {
+//        VERY USEFUL:         String a  = driver.getPageSource(); debug to view all the DOM content of a screen
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "127.0.0.1:5555");
         dc.setCapability("platformName", "android");
         dc.setCapability("appPackage","com.zhiliaoapp.musically");
         dc.setCapability("appActivity","com.ss.android.ugc.aweme.splash.SplashActivity");
-
         AndroidDriver<AndroidElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),dc);
 
-        MobileElement el3 = (MobileElement) driver.findElementById("com.zhiliaoapp.musically:id/ajq");
-        el3.click();
+        InitialScreen is = new InitialScreen(driver);
+
+        is.login();
 
         Assert.assertEquals(driver.findElementById("com.zhiliaoapp.musically:id/ad7").getText(),"Não conseguiu entrar?");
     }
